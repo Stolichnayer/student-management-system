08ec10298a1f6f6dec33e387050614e33a1aea26
Alex Perrakis <alexperrakis1@hotmail.com>
2021-02-06 23:56:47 +0200

    - Changed Grid header's titles - Implemented ClearDataInAddStudentPanel() - Reconstructed database and made primary key the regID, added gender field - Fixed all problems with focus, selection and enablement of remove student button

--- student-management-system/src/Forms/MainForm.Designer.cs
+++ student-management-system/src/Forms/MainForm.Designer.cs
@@ -36,6 +36,7 @@ namespace student_management_system.Forms
             this.addStudentButton = new System.Windows.Forms.Button();
             this.removeStudentButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.genderLabel = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.clearButton = new System.Windows.Forms.Button();
@@ -52,9 +53,18 @@ namespace student_management_system.Forms
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.totalStudentsLabel = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maleRadioButton = new System.Windows.Forms.RadioButton();
+            this.femaleRadioButton = new System.Windows.Forms.RadioButton();
+            this.otherRadioButton = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // addStudentButton
@@ -79,7 +89,11 @@ namespace student_management_system.Forms
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.genderLabel);
             this.panel1.Controls.Add(this.numericUpDown2);
             this.panel1.Controls.Add(this.dateTimePicker2);
             this.panel1.Controls.Add(this.clearButton);
@@ -97,15 +111,23 @@ namespace student_management_system.Forms
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(180, 21);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(320, 376);
+            this.panel1.Size = new System.Drawing.Size(336, 376);
             this.panel1.TabIndex = 5;
             this.panel1.Visible = false;
             this.panel1.Leave += new System.EventHandler(this.panel1_Leave);
             // 
+            // genderLabel
+            // 
+            this.genderLabel.Location = new System.Drawing.Point(38, 135);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(71, 18);
+            this.genderLabel.TabIndex = 19;
+            this.genderLabel.Text = "Gender";
+            // 
             // numericUpDown2
             // 
             this.numericUpDown2.DecimalPlaces = 2;
-            this.numericUpDown2.Location = new System.Drawing.Point(116, 203);
+            this.numericUpDown2.Location = new System.Drawing.Point(116, 232);
             this.numericUpDown2.Maximum = new decimal(new int[] {10, 0, 0, 0});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(95, 20);
@@ -115,7 +137,7 @@ namespace student_management_system.Forms
             // 
             this.dateTimePicker2.CustomFormat = "dd-MM-yyy";
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(116, 139);
+            this.dateTimePicker2.Location = new System.Drawing.Point(116, 168);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(95, 20);
             this.dateTimePicker2.TabIndex = 16;
@@ -155,7 +177,7 @@ namespace student_management_system.Forms
             // 
             this.dateTimePicker1.CustomFormat = "dd-MM-yyy";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(116, 167);
+            this.dateTimePicker1.Location = new System.Drawing.Point(116, 196);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(95, 20);
             this.dateTimePicker1.TabIndex = 11;
@@ -165,26 +187,26 @@ namespace student_management_system.Forms
             // 
             this.textBox4.Location = new System.Drawing.Point(116, 104);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(165, 20);
+            this.textBox4.Size = new System.Drawing.Size(176, 20);
             this.textBox4.TabIndex = 10;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(116, 74);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(165, 20);
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
             this.textBox2.TabIndex = 8;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(116, 40);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
             this.textBox1.TabIndex = 7;
             // 
             // label7
             // 
-            this.label7.Location = new System.Drawing.Point(38, 139);
+            this.label7.Location = new System.Drawing.Point(38, 168);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(72, 15);
             this.label7.TabIndex = 6;
@@ -192,7 +214,7 @@ namespace student_management_system.Forms
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(38, 205);
+            this.label6.Location = new System.Drawing.Point(38, 234);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(72, 15);
             this.label6.TabIndex = 5;
@@ -200,7 +222,7 @@ namespace student_management_system.Forms
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(38, 172);
+            this.label5.Location = new System.Drawing.Point(38, 201);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(72, 15);
             this.label5.TabIndex = 4;
@@ -238,11 +260,94 @@ namespace student_management_system.Forms
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowHeadersWidth = 30;
             this.dataGridView1.Size = new System.Drawing.Size(715, 452);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
             this.dataGridView1.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowLeave);
+            // 
+            // totalStudentsLabel
+            // 
+            this.totalStudentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.totalStudentsLabel.Location = new System.Drawing.Point(204, 474);
+            this.totalStudentsLabel.Name = "totalStudentsLabel";
+            this.totalStudentsLabel.Size = new System.Drawing.Size(186, 29);
+            this.totalStudentsLabel.TabIndex = 6;
+            this.totalStudentsLabel.Text = "Total students:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(7, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 20);
+            this.radioButton1.TabIndex = 20;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(71, 9);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 20);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Location = new System.Drawing.Point(128, 9);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 20);
+            this.radioButton3.TabIndex = 22;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Other";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.otherRadioButton);
+            this.groupBox1.Controls.Add(this.maleRadioButton);
+            this.groupBox1.Controls.Add(this.femaleRadioButton);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(115, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 28);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            // 
+            // maleRadioButton
+            // 
+            this.maleRadioButton.Checked = true;
+            this.maleRadioButton.Location = new System.Drawing.Point(5, 8);
+            this.maleRadioButton.Name = "maleRadioButton";
+            this.maleRadioButton.Size = new System.Drawing.Size(48, 18);
+            this.maleRadioButton.TabIndex = 21;
+            this.maleRadioButton.TabStop = true;
+            this.maleRadioButton.Text = "Male";
+            this.maleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // femaleRadioButton
+            // 
+            this.femaleRadioButton.Location = new System.Drawing.Point(57, 8);
+            this.femaleRadioButton.Name = "femaleRadioButton";
+            this.femaleRadioButton.Size = new System.Drawing.Size(62, 18);
+            this.femaleRadioButton.TabIndex = 22;
+            this.femaleRadioButton.Text = "Female";
+            this.femaleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // otherRadioButton
+            // 
+            this.otherRadioButton.Location = new System.Drawing.Point(124, 8);
+            this.otherRadioButton.Name = "otherRadioButton";
+            this.otherRadioButton.Size = new System.Drawing.Size(51, 18);
+            this.otherRadioButton.TabIndex = 23;
+            this.otherRadioButton.Text = "Other";
+            this.otherRadioButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -250,6 +355,7 @@ namespace student_management_system.Forms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(982, 566);
+            this.Controls.Add(this.totalStudentsLabel);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.removeStudentButton);
             this.Controls.Add(this.addStudentButton);
@@ -260,8 +366,21 @@ namespace student_management_system.Forms
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton maleRadioButton;
+        private System.Windows.Forms.RadioButton femaleRadioButton;
+        private System.Windows.Forms.RadioButton otherRadioButton;
+
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+
+        private System.Windows.Forms.Label totalStudentsLabel;
 
         private System.Windows.Forms.DataGridView dataGridView1;
 
--- student-management-system/src/Forms/MainForm.cs
+++ student-management-system/src/Forms/MainForm.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using student_management_system.Controller;
 
@@ -16,12 +18,21 @@ namespace student_management_system.Forms
         private void UpdateDataGrid()
         {
             const string query = "SELECT * FROM students";
-            
+
             DataSet dataset = DatabaseManager.GetInstance().ExecuteQuery(query);
 
             //Set DataGridView data source to our dataset
-            DataGridView1.DataSource = dataset.Tables[0]; 
-            
+            DataGridView1.DataSource = dataset.Tables[0];
+
+            totalStudentsLabel.Text = $"Total students: {DataGridView1.Rows.Count}";
+
+            DataGridView1.Columns[0].HeaderText = "Reg. ID";
+            DataGridView1.Columns[1].HeaderText = "First name";
+            DataGridView1.Columns[2].HeaderText = "Last name";
+            DataGridView1.Columns[3].HeaderText = "Email";
+            DataGridView1.Columns[4].HeaderText = "Birth date";
+            DataGridView1.Columns[5].HeaderText = "Reg. date";
+            DataGridView1.Columns[6].HeaderText = "GPA";
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
@@ -45,14 +56,20 @@ namespace student_management_system.Forms
                 panel1.Hide();
                 dataGridView1.Enabled = true;
             }
-                
+
         }
 
         private void clearButton_Click(object sender, EventArgs e)
-         {
+        {
+            ClearDataInAddStudentPanel();
+        }
+
+        private void ClearDataInAddStudentPanel()
+        {
             textBox1.Text = "";
             textBox2.Text = "";
             textBox4.Text = "";
+            maleRadioButton.Checked = true;
             numericUpDown2.Value = Decimal.Zero;
             dateTimePicker1.Value = DateTime.Now;
             dateTimePicker2.Value = new DateTime(2003, 01, 01);
@@ -61,17 +78,18 @@ namespace student_management_system.Forms
         private void addButton_Click(object sender, EventArgs e)
         {
             dataGridView1.Enabled = true;
-            
+
             string firstName = textBox1.Text;
             string lastName = textBox2.Text;
             string email = textBox4.Text;
+            string gender = maleRadioButton.Checked ? "Male" : femaleRadioButton.Checked ? "Female" : "Other";
             string birthDate = string.Format(dateTimePicker2.Value.ToString("yyyy-MM-dd"));
             string regDate = string.Format(dateTimePicker1.Value.ToString("yyyy-MM-dd"));
             string gpa = numericUpDown2.Text;
 
-            string query 
-                = "INSERT INTO students (firstName, lastName, email, birthDate, regDate, gpa) VALUES (" + 
-                  $"\"{firstName}\", \"{lastName}\", \"{email}\", \"{birthDate}\", \"{regDate}\", {gpa})";
+            string query
+                = "INSERT INTO students (firstName, lastName, email, gender, birthDate, regDate, gpa) VALUES (" +
+                  $"\"{firstName}\", \"{lastName}\", \"{email}\", \"{gender}\", \"{birthDate}\", \"{regDate}\", {gpa})";
 
             Console.Out.WriteLine(query);
             int result = DatabaseManager.GetInstance().ExecuteNonQuery(query);
@@ -85,13 +103,14 @@ namespace student_management_system.Forms
             {
                 UpdateDataGrid();
                 panel1.Hide();
+                ClearDataInAddStudentPanel();
                 dataGridView1.ClearSelection();
                 dataGridView1.Rows[^1].Selected = true;
                 dataGridView1.CurrentCell = dataGridView1.Rows[^1].Cells[^1];
                 MessageBox.Show("The student record was added in the database successfully.",
                     "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            
+
         }
 
         private void removeStudentButton_Click(object sender, EventArgs e)
@@ -100,7 +119,7 @@ namespace student_management_system.Forms
             var firstName = dataGridView1.CurrentRow?.Cells[1].Value.ToString();
             var lastName = dataGridView1.CurrentRow?.Cells[2].Value.ToString();
 
-            var result = MessageBox.Show($"Are you sure you want to delete student {firstName} {lastName}?", 
+            var result = MessageBox.Show($"Are you sure you want to delete student {firstName} {lastName}?",
                 "Confirmation", MessageBoxButtons.YesNo);
 
             if (result == DialogResult.Yes)
@@ -116,13 +135,13 @@ namespace student_management_system.Forms
 
         private void dataGridView1_Leave(object sender, EventArgs e)
         {
-            
+
         }
 
         private void dataGridView1_RowLeave(object sender, DataGridViewCellEventArgs e)
         {
             removeStudentButton.Enabled = false;
-     
+
         }
 
         private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
@@ -132,7 +151,7 @@ namespace student_management_system.Forms
 
         private void panel1_Leave(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
